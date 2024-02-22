a3ca2e3721b19b638c93bc7cc5372ed5ada32232
mcavol <127151698+mcavol@users.noreply.github.com>
2024-02-22 04:35:56 +0200

    Add files via upload

---
+++ week 2/pagerank/pagerank/pagerank.py
@@ -0,0 +1,146 @@
+import os
+import random
+import re
+import sys
+
+DAMPING = 0.85
+SAMPLES = 10000
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+
+
+def crawl(directory):
+    """
+    Parse a directory of HTML pages and check for links to other pages.
+    Return a dictionary where each key is a page, and values are
+    a list of all other pages in the corpus that are linked to by the page.
+    """
+    pages = dict()
+
+    # Extract all links from HTML files
+    for filename in os.listdir(directory):
+        if not filename.endswith(".html"):
+            continue
+        with open(os.path.join(directory, filename)) as f:
+            contents = f.read()
+            links = re.findall(r"<a\s+(?:[^>]*?)href=\"([^\"]*)\"", contents)
+            pages[filename] = set(links) - {filename}
+
+    # Only include links to other pages in the corpus
+    for filename in pages:
+        pages[filename] = set(
+            link for link in pages[filename]
+            if link in pages
+        )
+
+    return pages
+
+
+def transition_model(corpus, page, damping_factor):
+    """
+    Return a probability distribution over which page to visit next,
+    given a current page.
+
+    With probability `damping_factor`, choose a link at random
+    linked to by `page`. With probability `1 - damping_factor`, choose
+    a link at random chosen from all pages in the corpus.
+    """
+    probs = {}
+    links = corpus[page]
+    free_links = []
+    for page in corpus:
+        free_links.append(page)
+        probs[page] = 0
+    
+    for link in links:
+        probs[link] = (damping_factor)*(1/len(links))
+
+    for link in free_links:
+        probs[link] = probs[link] + ((1 - damping_factor) * (1/len(free_links)))
+
+    return probs
+
+    
+
+def sample_pagerank(corpus, damping_factor, n):
+    """
+    Return PageRank values for each page by sampling `n` pages
+    according to transition model, starting with a page at random.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    pages = list(corpus.keys())
+    dictionary = {}
+    first_choice = random.choice(pages)
+    for page in pages:
+        dictionary[page] = 0
+
+    dictionary[first_choice] = 1/n
+    current_probs = transition_model(corpus, first_choice, damping_factor)
+    for i in range(n-1):
+        next_page = random.choices(list(current_probs.keys()), list(current_probs.values()), k=1)
+        dictionary[next_page[0]] = dictionary[next_page[0]] + 1/n
+        current_probs = transition_model(corpus,next_page[0], damping_factor)
+
+    return dictionary
+
+
+def iterate_pagerank(corpus, damping_factor):
+    """
+    Return PageRank values for each page by iteratively updating
+    PageRank values until convergence.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    num_pages = len(corpus)
+    dict1 = {}
+    dict2 = {}
+
+    
+    for page in corpus:
+        dict1[page] = 1 / num_pages
+
+    
+    while True:
+        for page in corpus:
+            temp = 0
+            for linking_page in corpus:
+                
+                if page in corpus[linking_page]:
+                    temp += (dict1[linking_page] / len(corpus[linking_page]))
+                
+                if len(corpus[linking_page]) == 0:
+                    temp += (dict1[linking_page]) / len(corpus)
+            temp *= damping_factor
+            temp += (1 - damping_factor) / num_pages
+
+            dict2[page] = temp
+
+        difference = max([abs(dict2[x] - dict1[x]) for x in dict1])
+        if difference < 0.001:
+            break
+        else:
+            dict1 = dict2.copy()
+
+    return dict1
+
+
+
+if __name__ == "__main__":
+    main()
